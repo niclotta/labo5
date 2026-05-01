52680140f162c2aeea3f04fd1a45a87f78b7333e
Nicolás Clotta <nicolas.clotta@gmail.com>
2026-05-01 16:46:16 -0300

    crear dirs para las imgs si no existen

--- trackeo.py
+++ trackeo.py
@@ -6,12 +6,13 @@ Created on Tue Apr 21 20:16:22 2026
 @author: nclotta
 """
 
-# Time-stamp: </Users/nclotta/Documents/__UBA/__LABO_5_CINCO/pinzas/trackeo.py, 2026-05-01 Friday 16:26:43 nclotta>
+# Time-stamp: </Users/nclotta/Documents/__UBA/__LABO_5_CINCO/pinzas/trackeo.py, 2026-05-01 Friday 16:45:18 nclotta>
 
 import numpy as np
 import matplotlib.pyplot as plt
 import trackpy as tp
 import pims
+import os
 
 from pathlib import Path
 
@@ -24,7 +25,7 @@ import datasets
 def g(img):
     return img[:, :, 1]
 
-def track_vid(v_in, n, data, graph=False):
+def track_vid(v_in, n, data, graph=False, imgpath="./img"):
   frames = pims.open(v_in)
   tp.quiet()
   q = tp.link(tp.batch(list(g(frames)), invert=True,
@@ -33,7 +34,7 @@ def track_vid(v_in, n, data, graph=False):
                          minmass=data[n][2]), data[n][0]*data[n][1], memory=40)
   if graph:
     plt.hist(q['particle'], bins=q['particle'].max(), ec='r', color='skyblue')
-    plt.savefig(f"./img/{Path(v_in).stem}_histo_parts.png", dpi=300)
+    plt.savefig(f"{imgpath}/{Path(v_in).stem}_histo_parts.png", dpi=300)
     plt.close()
     if len(data[n][3]) > 0:
       n_parts = len(data[n][3])
@@ -49,7 +50,7 @@ def track_vid(v_in, n, data, graph=False):
       tp.annotate(q[(q['particle'] == j) &
                     (q['frame'] == fnum)], frames[fnum], ax=ax1)
       tp.plot_traj(q[q['particle'] == j], ax=ax2)
-      plt.savefig(f"./img/{Path(v_in).stem}_part_{j}.png", dpi=300)
+      plt.savefig(f"{imgpath}/{Path(v_in).stem}_part_{j}.png", dpi=300)
       plt.close()
   return q
 
@@ -57,6 +58,9 @@ if __name__ == '__main__':
   for dataset in datasets.todo:
     for n in range(len(dataset[0])):
       if dataset[0][n][0] > 0:
-        track_vid(f"data/{dataset[1]}_{n}.avi", n, dataset[0], graph=True)
+        if not os.path.exists(f"./img/{dataset[1]}"):
+          os.makedirs(f"./img/{dataset[1]}")
+        track_vid(f"data/{dataset[1]}_{n}.avi", n, dataset[0],
+                    graph=True, imgpath=f"./img/{dataset[1]}")
 
 # eof
